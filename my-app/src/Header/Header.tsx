745098b4b5c0c6bdfe666b7b5a4f298df34fba83
m1rojke <mirojkeeeee@gmail.com>
2023-09-19 19:00:32 +0300

    dfdf

---
+++ my-app/src/Header/Header.tsx
@@ -0,0 +1,17 @@
+function Header () {
+  return (
+    <header>
+      <img src="" alt="" />
+      <nav>
+        <a>Home</a>
+        <a>Categories</a>
+        <a>About us</a>
+      </nav>
+      <div>
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
+    </header>
+  )
+}
